618a88ca869676ac15ea1496f789d77b36542baf
VariantF <sunjiayu_2006@126.com>
2012-12-13 15:46:32 +0800

    ==

---
+++ Moo.FullIndex/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moo.FullIndex
+{
+    public class vFullIndex : Moo.Core.Daemon.Daemon
+    {
+        protected override int Run()
+        {
+            Console.Beep();
+            return 0;
+        }
+    }
+}
