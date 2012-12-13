85a0ee458eac23ee878a6b4ff3aa1b9db3de7df1
onetwogoo <onetwogoo@live.com>
2012-12-13 19:36:50 +0800

    IndexAPI 开始，加入题目

---
+++ Moo.Core/IndexAPI/IndexItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moo.Core.IndexAPI
+{
+    public class IndexItem
+    {
+        public int ID;
+        public IEnumerable<string> Keywords;
+        public string Content;
+    }
+}
